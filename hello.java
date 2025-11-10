ccb7d04a5f4802e383d755147ac1d56df9ce291a
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2025-11-10 12:40:24 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,17 @@
+//print pattern 
+        // 5
+        // 55
+        // 555
+        // 5555
+
+class main{
+    public static void main(String[] args) {
+        int n=4;
+        for(int i = 1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("5");
+            }
+            System.out.println();
+        }
+    }
+}    
